17ffb26894dfccbd3fccc601c8b63ac5dab70ea9
bmincof <bminlvcoffee@gmail.com>
2022-11-24 23:45:36 +0900

    TitleMaker 및 Impl 구현

--- src/main/java/component/TitleMakerImpl.java
+++ src/main/java/component/TitleMakerImpl.java
@@ -1,21 +1,66 @@
 package component;
 
 import interfaces.TitleMaker;
+import lombok.Builder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
+@Builder
 public class TitleMakerImpl implements TitleMaker {
 
-    private String titleFormat = "[n일차] day.md";
-    private String TILday = "10";
-    private String today = LocalDate.now().format(DateTimeFormatter.ofPattern("d"));
+    private final String titleFormat;
+    private final String github;
 
     @Override
-    public String makeTitle(){
+    public String makeTitle() {
+        try{
+            return replaceFormat(loadSeqInfo());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-        return titleFormat.replace("n",TILday).replace("day",today);
+    private String replaceFormat(String format) {
+        return format.replace("day", String.valueOf(LocalDate.now().getDayOfMonth()));
+    }
+
+    private String loadSeqInfo() throws IOException {
+        URL url = new URL("https://api.github.com/repos/bmincof/TIL/commits");
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-Type", "application/json"); // Content-Type 지정
+        conn.setDoOutput(true); // 출력 가능 상태로 변경
+        conn.connect();
+
+// 데이터  읽어오기
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+        while((line = br.readLine()) != null) {
+            sb.append(line); // StringBuilder 사용시 객체를 계속 생성하지 않고 하나의 객체릂 수정하므로 더 빠름.
+        }
+        conn.disconnect();
+
+// JSON Parsing
+        String json = sb.toString();
+
+        JSONArray commits = new JSONArray(json);
+        String message = commits.getJSONObject(0).getJSONObject("commit").getString("message");
+
+        int startIdx = message.indexOf("[");
+        int endIdx = message.indexOf("일");
+        String dayInfo = message.substring(startIdx + 1, endIdx);
+
+        return titleFormat.replace("seq",Integer.parseInt(dayInfo) + 1 + "");
     }
 
 }
--- src/test/java/component/TitleMakerImplTest.java
+++ src/test/java/component/TitleMakerImplTest.java
@@ -1,22 +1,42 @@
 package component;
 
+import interfaces.TitleMaker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.*;
 
 class TitleMakerImplTest {
 
-    @DisplayName("제목 생성 테스트")
+    @DisplayName("TitleMaker 생성 테스트")
     @Test
-    public void titleMake() {
-        final String expected = "[10일차] 3.md";
-        final TitleMakerImpl titleMaker = new TitleMakerImpl();
+    public void testMakeTitleMaker() {
+        final String titleFormat = "[seq일차] day.md";
+        final String github = "https://github.com/bmincof/TIL-Maker";
+        final TitleMaker sut = TitleMakerImpl.builder()
+                .titleFormat(titleFormat)
+                .github(github)
+                .build();
 
-        final String actual = titleMaker.makeTitle();
+        assertThat(sut).hasFieldOrPropertyWithValue("titleFormat", titleFormat)
+                .hasFieldOrPropertyWithValue("github", github);
+    }
+    @DisplayName("title 생성")
+    @Test
+    public void testMakeTitleWithGithub() {
+        final String titleFormat = "[seq일차] day.md";
+        final String github = "https://github.com/bmincof/TIL";
+        final TitleMaker sut = TitleMakerImpl.builder()
+                .titleFormat(titleFormat)
+                .github(github)
+                .build();
+        final String today = String.valueOf(LocalDate.now().getDayOfMonth());
+
+        final String expected = "[41일차] " + today +".md";
+        final String actual = sut.makeTitle();
 
         assertThat(actual).isEqualTo(expected);
-
     }
-
 }
